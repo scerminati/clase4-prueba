a3f0860f5eef6ad0eb55f37ee51127802f96997b
Sofia Cerminati <sofiacermi@hotmail.com>
2023-12-22 18:04:05 -0300

    Commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/scerminati/clase4-prueba",
+  base: "/clase4-prueba/",
   plugins: [react()],
 });
